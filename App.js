398b89ee92789e5c020d7c9c13c58b141067ca6b
malena <89429614+malenarivera@users.noreply.github.com>
2024-05-21 00:26:31 -0300

    Agrego punto 3 y 4 del tp

---
+++ App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View } from 'react-native';
+import CervezaLista from './components/cervezaLista';
+import CervezaDetalle from './components/cervezaDetalle';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CervezaLista">
+        <Stack.Screen 
+          name="CervezaLista" 
+          component={CervezaLista} 
+          options={{ title: 'Bienvenidx!, qué vas a tomar hoy?' }} 
+        />
+        <Stack.Screen 
+          name="CervezaDetalle" 
+          component={CervezaDetalle} 
+          options={{ title: 'Detalle de la Cerveza' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
